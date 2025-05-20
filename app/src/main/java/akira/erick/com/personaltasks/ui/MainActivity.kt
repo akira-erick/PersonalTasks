125dc37ecc7181610f14f6286901c2d75c387170
akira-erick <kajitani.motta@gmail.com>
2025-05-20 18:54:59 -0300

    feat(layout): changing layout to layoutInflater

--- app/src/main/java/akira/erick/com/personaltasks/ui/MainActivity.kt
+++ app/src/main/java/akira/erick/com/personaltasks/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package akira.erick.com.personaltasks.ui
 
 import akira.erick.com.personaltasks.R
+import akira.erick.com.personaltasks.databinding.ActivityMainBinding
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,14 +9,18 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        setContentView(amb.root)
+        setSupportActionBar(amb.toolbarIn.toolbar)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
