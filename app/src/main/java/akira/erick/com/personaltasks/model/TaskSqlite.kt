bd3c0d9a351a1d54ae58357cf88e6f165176950c
akira-erick <kajitani.motta@gmail.com>
2025-05-22 18:18:36 -0300

    feat(task-sqlite): adding sqlite task table creating

--- app/src/main/java/akira/erick/com/personaltasks/model/TaskSqlite.kt
+++ app/src/main/java/akira/erick/com/personaltasks/model/TaskSqlite.kt
@@ -3,6 +3,21 @@ package akira.erick.com.personaltasks.model
 import android.content.Context
 
 class TaskSqlite(context: Context): TaskDao {
+    companion object{
+        private const val TASK_DATABASE_FILE = "taskList"
+        private const val TASK_TABLE = "task"
+        private const val ID_COLUMN = "id"
+        private const val TITLE_COLUMN = "title"
+        private const val DESCRIPTION_COLUMN = "description"
+        private const val DEADLINE_COLUMN = "deadline"
+
+        const val CREATE_TASK_TABLE_STATEMENT = "CREATE TABLE IF NOT EXISTS $TASK_TABLE (" +
+                "$ID_COLUMN INTEGER NOT NULL PRIMARY KEY, " +
+                "$TITLE_COLUMN TEXT NOT NULL, " +
+                "$DESCRIPTION_COLUMN TEXT NOT NULL, " +
+                "$DEADLINE_COLUMN TEXT NOT NULL );"
+    }
+
     override fun createTask(task: Task): Long {
         TODO("Not yet implemented")
     }
