b58a0c13d81aff8d762e1e3aceb68d4f4a6d89a0
akira-erick <kajitani.motta@gmail.com>
2025-05-22 18:24:22 -0300

    feat(task-sqlite): initializing database each time if needed

--- app/src/main/java/akira/erick/com/personaltasks/model/TaskSqlite.kt
+++ app/src/main/java/akira/erick/com/personaltasks/model/TaskSqlite.kt
@@ -1,6 +1,11 @@
 package akira.erick.com.personaltasks.model
 
+import akira.erick.com.personaltasks.R
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.database.sqlite.SQLiteDatabase
+import android.util.Log
+import java.sql.SQLException
 
 class TaskSqlite(context: Context): TaskDao {
     companion object{
@@ -16,6 +21,21 @@ class TaskSqlite(context: Context): TaskDao {
                 "$TITLE_COLUMN TEXT NOT NULL, " +
                 "$DESCRIPTION_COLUMN TEXT NOT NULL, " +
                 "$DEADLINE_COLUMN TEXT NOT NULL );"
+    }
+
+    private val taskDatabase: SQLiteDatabase = context.openOrCreateDatabase(
+        TASK_DATABASE_FILE,
+        MODE_PRIVATE,
+        null
+    )
+
+    init{
+        try{
+            taskDatabase.execSQL(CREATE_TASK_TABLE_STATEMENT)
+        }
+        catch (se: SQLException) {
+            Log.e(context.getString(R.string.app_name), se.message.toString())
+        }
     }
 
     override fun createTask(task: Task): Long {
