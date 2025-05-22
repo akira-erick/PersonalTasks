8d24cba52faa2245ba7fd4c3146005da4554bb68
akira-erick <kajitani.motta@gmail.com>
2025-05-22 19:13:14 -0300

    feat(task-persistence): add controller of tasks to handle tasks CRUD

---
+++ app/src/main/java/akira/erick/com/personaltasks/controller/MainController.kt
@@ -0,0 +1,16 @@
+package akira.erick.com.personaltasks.controller
+
+import akira.erick.com.personaltasks.model.Task
+import akira.erick.com.personaltasks.model.TaskDao
+import akira.erick.com.personaltasks.model.TaskSqlite
+import akira.erick.com.personaltasks.ui.MainActivity
+
+class MainController(mainActivity: MainActivity) {
+    private val taskDao: TaskDao = TaskSqlite(mainActivity)
+
+    fun insertTask(task: Task) = taskDao.createTask(task)
+    fun getTask(id: Int) = taskDao.retrieveTask(id)
+    fun getTasks() = taskDao.retrieveTasks()
+    fun modifyTask(task: Task) = taskDao.updateTask(task)
+    fun removeTask(id: Int) = taskDao.deleteTask(id)
+}
