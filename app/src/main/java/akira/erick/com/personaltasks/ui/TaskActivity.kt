f99864cb160c7929dcfa5bae76e65cb747f40809
akira-erick <kajitani.motta@gmail.com>
2025-05-22 11:19:25 -0300

    feat(task-activity): preparing TaskActivity

--- app/src/main/java/akira/erick/com/personaltasks/ui/TaskActivity.kt
+++ app/src/main/java/akira/erick/com/personaltasks/ui/TaskActivity.kt
@@ -1,4 +1,12 @@
 package akira.erick.com.personaltasks.ui
 
-class TaskActivity {
+import akira.erick.com.personaltasks.databinding.ActivityTaskBinding
+import androidx.appcompat.app.AppCompatActivity
+
+class TaskActivity : AppCompatActivity() {
+    private val acb: ActivityTaskBinding by lazy {
+        ActivityTaskBinding.inflate(layoutInflater)
+    }
+
+
 }
