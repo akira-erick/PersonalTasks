8556a101706b301ea80c1786917f2182253d4430
akira-erick <kajitani.motta@gmail.com>
2025-06-26 18:47:54 -0300

    feat(): add TaskFirbaseDatabase to handle remote persistence

---
+++ app/src/main/java/akira/erick/com/personaltasks/model/TaskFirebaseDatabase.kt
@@ -0,0 +1,24 @@
+package akira.erick.com.personaltasks.model
+
+class TaskFirebaseDatabase: TaskDao {
+
+    override fun createTask(task: Task): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun retrieveTask(id: Int): Task {
+        TODO("Not yet implemented")
+    }
+
+    override fun retrieveTasks(): MutableList<Task> {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateTask(task: Task): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteTask(id: Int): Int {
+        TODO("Not yet implemented")
+    }
+}
