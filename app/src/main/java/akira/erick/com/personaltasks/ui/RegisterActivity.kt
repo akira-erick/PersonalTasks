b9dae2d8436bed24e9eb027a7393e0d7b80b5fb8
akira-erick <kajitani.motta@gmail.com>
2025-06-26 19:39:22 -0300

    feat(): add RegisterActivity, connecting it with the layout and writing in AndroidManifest

---
+++ app/src/main/java/akira/erick/com/personaltasks/ui/RegisterActivity.kt
@@ -0,0 +1,10 @@
+package akira.erick.com.personaltasks.ui
+
+import akira.erick.com.personaltasks.databinding.ActivityRegisterBinding
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+    private val arb: ActivityRegisterBinding by lazy {
+        ActivityRegisterBinding.inflate(layoutInflater)
+    }
+}
