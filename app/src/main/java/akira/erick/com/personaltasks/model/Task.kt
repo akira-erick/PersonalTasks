b5b0a6adc602a52c921fba40d751953a8fe05408
akira-erick <kajitani.motta@gmail.com>
2025-06-27 01:19:41 -0300

    fix(): changing values to fix

--- app/src/main/java/akira/erick/com/personaltasks/model/Task.kt
+++ app/src/main/java/akira/erick/com/personaltasks/model/Task.kt
@@ -2,10 +2,14 @@ package akira.erick.com.personaltasks.model
 
 import akira.erick.com.personaltasks.model.Constant.INVALID_TASK_ID
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Entity
 data class Task(
+    @PrimaryKey(autoGenerate = true)
     var id: Int? = INVALID_TASK_ID,
     var title: String = "",
     var description: String = "",
