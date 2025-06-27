17000c6e15b08761bd7f8dc49ada10483e23b550
akira-erick <kajitani.motta@gmail.com>
2025-06-27 04:46:00 -0300

    feat(): add initial implementation of DeletedTaskListActivity

--- app/src/main/java/akira/erick/com/personaltasks/controller/DeletedTaskController.kt
+++ app/src/main/java/akira/erick/com/personaltasks/controller/DeletedTaskController.kt
@@ -4,44 +4,37 @@ import akira.erick.com.personaltasks.model.Constant.EXTRA_TASK_ARRAY
 import akira.erick.com.personaltasks.model.Task
 import akira.erick.com.personaltasks.model.TaskDao
 import akira.erick.com.personaltasks.model.TaskFirebaseDatabase
-import akira.erick.com.personaltasks.model.TaskSqlite
-import akira.erick.com.personaltasks.ui.MainActivity
+import akira.erick.com.personaltasks.ui.DeletedTaskListActivity
 import android.os.Message
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class MainController(private val mainActivity: MainActivity) {
+class DeletedTaskController(private val deletedTaskListActivity: DeletedTaskListActivity) {
+
     private val taskDao: TaskDao = TaskFirebaseDatabase()
     private val databaseCoroutineScope = CoroutineScope(Dispatchers. IO)
 
-    fun insertTask(task: Task) {
-        databaseCoroutineScope.launch {
-            taskDao.createTask(task)
-        }
-    }
-
-    fun getTask(id: Int) = taskDao.retrieveTask(id)
-
-    fun getTasks(){
-        databaseCoroutineScope.launch {
-            val taskList = taskDao.retrieveTasks().filter{ !it.is_deleted }
-            mainActivity.getTasksHandler.sendMessage(Message().apply {
+    fun getDeletedTasks(){
+        databaseCoroutineScope.launch{
+            val taskList = taskDao.retrieveTasks().filter { it.is_deleted }
+            deletedTaskListActivity.getDeletedTaskHandler.sendMessage(Message().apply {
                 data.putParcelableArray(EXTRA_TASK_ARRAY, taskList.toTypedArray())
             })
         }
     }
 
-    fun modifyTask(task: Task){
-        databaseCoroutineScope.launch {
-            taskDao.updateTask(task)
-        }
-    }
-
-    fun removeTask(task: Task){
-        task.is_deleted = true;
+    fun activateTask(task: Task){
+        task.is_deleted = false;
         databaseCoroutineScope.launch{
             taskDao.updateTask(task)
         }
     }
+
+    fun deleteTask(task: Task){
+        databaseCoroutineScope.launch{
+            taskDao.deleteTask(task)
+        }
+    }
+
 }
--- app/src/main/java/akira/erick/com/personaltasks/ui/DeletedTaskListActivity.kt
+++ app/src/main/java/akira/erick/com/personaltasks/ui/DeletedTaskListActivity.kt
@@ -1,13 +1,72 @@
 package akira.erick.com.personaltasks.ui
 
+import akira.erick.com.personaltasks.adapter.DeletedTaskRvAdapter
+import akira.erick.com.personaltasks.controller.DeletedTaskController
+import akira.erick.com.personaltasks.controller.MainController
 import akira.erick.com.personaltasks.databinding.ActivityDeletedTaskListBinding
+import akira.erick.com.personaltasks.model.Constant.EXTRA_TASK_ARRAY
 import akira.erick.com.personaltasks.model.Task
+import akira.erick.com.personaltasks.ui.MainActivity.Companion
+import android.content.Intent
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 
-class DeletedTaskListActivity : AppCompatActivity(){
+class DeletedTaskListActivity : AppCompatActivity(), OnDeletedTaskClickListener{
     private val alb: ActivityDeletedTaskListBinding by lazy {
         ActivityDeletedTaskListBinding.inflate(layoutInflater)
     }
 
     private val taskList: MutableList<Task> = mutableListOf()
+
+    private val taskAdapter: DeletedTaskRvAdapter by lazy {
+        DeletedTaskRvAdapter(taskList, this)
+    }
+
+    private lateinit var carl: ActivityResultLauncher<Intent>
+
+    private val deletedTaskController: DeletedTaskController by lazy {
+        DeletedTaskController(this)
+    }
+
+    companion object {
+        const val GET_TASKS_MESSAGE = 1
+        const val GET_TASKS_INTERVAL = 2000L
+    }
+    val getDeletedTaskHandler = object: Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message){
+            super.handleMessage(msg)
+            if (msg.what == GET_TASKS_MESSAGE) {
+                deletedTaskController.getDeletedTasks()
+                sendMessageDelayed(
+                    obtainMessage().apply { what = MainActivity.GET_TASKS_MESSAGE },
+                    MainActivity.GET_TASKS_INTERVAL
+                )
+            } else {
+                val taskArray = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    msg.data?.getParcelableArray(EXTRA_TASK_ARRAY, Task::class.java)
+                } else {
+                    msg.data?.getParcelableArray(EXTRA_TASK_ARRAY)
+                }
+                taskList.clear()
+                taskArray?.forEach { taskList.add(it as Task) }
+                taskAdapter.notifyDataSetChanged()
+            }
+        }
+    }
+
+    override fun onTaskClick(position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onActivateTaskMenuItemClick(position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onVisualizationMenuItemClick(position: Int) {
+        TODO("Not yet implemented")
+    }
 }
