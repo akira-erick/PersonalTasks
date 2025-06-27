039c2a5fee0d9f23760909c448862fc5f9b5cd29
akira-erick <kajitani.motta@gmail.com>
2025-06-27 04:32:55 -0300

    feat(): add implementation to DeletedTaskRvAdapter

--- app/src/main/java/akira/erick/com/personaltasks/adapter/DeletedTaskRvAdapter.kt
+++ app/src/main/java/akira/erick/com/personaltasks/adapter/DeletedTaskRvAdapter.kt
@@ -1,4 +1,67 @@
 package akira.erick.com.personaltasks.adapter
 
-class DeletedTaskRvAdapter {
+import akira.erick.com.personaltasks.R
+import akira.erick.com.personaltasks.databinding.TileDeletedTaskBinding
+import akira.erick.com.personaltasks.model.Task
+import akira.erick.com.personaltasks.ui.OnDeletedTaskClickListener
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+
+class DeletedTaskRvAdapter(
+    private val taskList: MutableList<Task>,
+    private val onDeletedTaskClickListener: OnDeletedTaskClickListener
+): RecyclerView.Adapter<DeletedTaskRvAdapter.DeletedTaskViewHolder>() {
+    inner class DeletedTaskViewHolder(tdtb: TileDeletedTaskBinding): RecyclerView.ViewHolder(tdtb.root){
+        val titleTv: TextView = tdtb.titleTv
+        val descriptionTv: TextView = tdtb.descriptionTv
+        val deadlineTv: TextView = tdtb.deadlineTv
+
+        init {
+            tdtb.root.setOnCreateContextMenuListener{ menu, v, menuInfo ->
+                (onDeletedTaskClickListener as AppCompatActivity).menuInflater.inflate(R.menu.context_deleted_tasks, menu)
+                menu.findItem(R.id.visualize_task_mi).setOnMenuItemClickListener {
+                    onDeletedTaskClickListener.onActivateTaskMenuItemClick(adapterPosition)
+                    true
+                }
+                menu.findItem(R.id.reactivate_task_mi).setOnMenuItemClickListener {
+                    onDeletedTaskClickListener.onVisualizationMenuItemClick(adapterPosition)
+                    true
+                }
+            }
+
+            tdtb.root.setOnClickListener {
+                onDeletedTaskClickListener.onTaskClick(adapterPosition)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): DeletedTaskViewHolder = DeletedTaskViewHolder(
+        TileDeletedTaskBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+    )
+
+    override fun getItemCount(): Int = taskList.size
+
+    override fun onBindViewHolder(
+        holder: DeletedTaskViewHolder,
+        position: Int
+    ) {
+        taskList[position].let { task ->
+            with(holder) {
+                titleTv.text = task.title
+                descriptionTv.text = task.description
+                deadlineTv.text = task.deadline
+            }
+        }
+    }
+
 }
--- app/src/main/java/akira/erick/com/personaltasks/ui/DeletedTaskListActivity.kt
+++ app/src/main/java/akira/erick/com/personaltasks/ui/DeletedTaskListActivity.kt
@@ -1,7 +1,13 @@
 package akira.erick.com.personaltasks.ui
 
+import akira.erick.com.personaltasks.databinding.ActivityDeletedTaskListBinding
+import akira.erick.com.personaltasks.model.Task
 import androidx.appcompat.app.AppCompatActivity
 
 class DeletedTaskListActivity : AppCompatActivity(){
+    private val alb: ActivityDeletedTaskListBinding by lazy {
+        ActivityDeletedTaskListBinding.inflate(layoutInflater)
+    }
 
+    private val taskList: MutableList<Task> = mutableListOf()
 }
