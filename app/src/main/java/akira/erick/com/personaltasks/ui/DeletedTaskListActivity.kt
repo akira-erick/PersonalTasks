9e6c4065042c0c25d7628d42f4b398fc669b3f7e
akira-erick <kajitani.motta@gmail.com>
2025-06-27 05:01:07 -0300

    feat(): implementing activate task item click and creation of DeletedTaskListActivity

--- app/src/main/java/akira/erick/com/personaltasks/ui/DeletedTaskListActivity.kt
+++ app/src/main/java/akira/erick/com/personaltasks/ui/DeletedTaskListActivity.kt
@@ -1,19 +1,28 @@
 package akira.erick.com.personaltasks.ui
 
+import akira.erick.com.personaltasks.R
 import akira.erick.com.personaltasks.adapter.DeletedTaskRvAdapter
 import akira.erick.com.personaltasks.controller.DeletedTaskController
 import akira.erick.com.personaltasks.controller.MainController
 import akira.erick.com.personaltasks.databinding.ActivityDeletedTaskListBinding
+import akira.erick.com.personaltasks.model.Constant.EXTRA_TASK
 import akira.erick.com.personaltasks.model.Constant.EXTRA_TASK_ARRAY
 import akira.erick.com.personaltasks.model.Task
 import akira.erick.com.personaltasks.ui.MainActivity.Companion
 import android.content.Intent
 import android.os.Build
+import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
+import android.view.Menu
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class DeletedTaskListActivity : AppCompatActivity(), OnDeletedTaskClickListener{
     private val alb: ActivityDeletedTaskListBinding by lazy {
@@ -58,15 +67,38 @@ class DeletedTaskListActivity : AppCompatActivity(), OnDeletedTaskClickListener{
         }
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(alb.root)
+        setSupportActionBar(alb.toolbarIn.toolbar)
+        supportActionBar?.subtitle = getString(R.string.deleted_task_list)
+
+
+        alb.taskRv.adapter = taskAdapter
+        alb.taskRv.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
     override fun onTaskClick(position: Int) {
         TODO("Not yet implemented")
     }
 
     override fun onActivateTaskMenuItemClick(position: Int) {
-        TODO("Not yet implemented")
+        deletedTaskController.activateTask(taskList[position])
+        taskList.removeAt(position)
+        taskAdapter.notifyItemRemoved(position)
+        Toast.makeText(this, "Task activated!", Toast.LENGTH_SHORT).show()
     }
 
     override fun onVisualizationMenuItemClick(position: Int) {
         TODO("Not yet implemented")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Firebase.auth.currentUser == null) finish()
     }
 }
